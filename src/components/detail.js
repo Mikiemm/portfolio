378c1113865ed7fe8b2a659fd365718ee19aa628
Mikiemm <svid.marketa@gmail.com>
2019-02-23 18:12:35 +0100

    Update detail.js

--- src/components/detail.js
+++ src/components/detail.js
@@ -146,7 +146,7 @@ const Content = styled.main`
     line-height: 1.59;
     letter-spacing: 0.75px;
     margin: 0;
-    padding: 5px 0 25px;
+    padding: 5px 0 5px;
     color: ${colors.black};
   }
 
