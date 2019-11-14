4a4007fb39daa981e7c743b337f3f75d2beca2dc
Mikiemm <svid.marketa@gmail.com>
2019-11-14 20:06:42 +0100

    Update underline.js

--- src/components/underline.js
+++ src/components/underline.js
@@ -4,7 +4,7 @@ export const activeStyle = 'background-size: 100% 100%'
 
 const Underline = styled.span`
   width: calc(100%);
-  background-image: linear-gradient(transparent calc(100% - 0.10em), currentColor 10px);
+  background-image: linear-gradient(transparent calc(100% - 0.07em), currentColor 10px);
   background-repeat: no-repeat;
   background-size: 0% 100%;
   transition: background-size 0.6s;
