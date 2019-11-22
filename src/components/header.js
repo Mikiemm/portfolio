81715292c9ee16f683a609875f2f90adc32f3811
Mikiemm <svid.marketa@gmail.com>
2019-11-22 23:04:29 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -35,12 +35,7 @@ export default () => (
             Resume
           </Remote>
           <Remote rel="noopener" href="https://dribbble.com/marketa" target="_blank">
-            <svg xmlns='http://www.w3.org/2000/svg' width='13' height='13' viewBox='0 0 13 13'>
-                <g fill='none' fillRule='evenodd'>
-                    <path d='M-1-1h15v15H-1z' />
-                    <path fill='currentColor' fillRule='nonzero' d='M10.875 10.875h-8.75v-8.75H6.5V.875H.875v11.25h11.25V6.5h-1.25v4.375zM7.75.875v1.25h2.244L3.85 8.269l.881.881 6.144-6.144V5.25h1.25V.875H7.75z'
-                    />
-                </g>
+            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16'>
             </svg>
           </Remote>
           <Remote rel="noopener" href="https://www.linkedin.com/in/marketa-svid" target="_blank">
